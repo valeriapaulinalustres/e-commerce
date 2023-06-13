ad23b589f4ae1fd08170ef039b497398cf9188bc
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2023-06-13 14:35:43 -0300

    error

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -31,7 +31,7 @@ export const getProductsController = async (req, res) => {
     res.json({ response: products }); //esta se usará con el front de React
     //res.render("products", { products, userName });
   } catch (error) {
-    console.log('error')
+    console.log('error', error)
    // logger.error('Error del controller', error)
   }
 };
